81e6a8cb08dfbae6c2bc3a89ece06d003c61d7f4
Marius <geciauskasmarius@gmail.com>
2015-07-19 15:50:26 +0300

    initial commit

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+
+@section('content')
+<div class="container content_padding_top posts-container">
+
+
+    <div class="col-lg-6 ">
+      
+
+            <h4 class="text-center post-heading">Paieškos rezultatai</h4>
+            
+         
+    </div>
+
+</div>
+
+@endsection
